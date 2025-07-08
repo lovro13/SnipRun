517897f6121bb5a076e35eca04edf9333a915ba3
Lovro <lovro.zskrlj@gmail.com>
2025-07-08 13:27:09 +0200

    init java

---
+++ src/main/java/SnipRun/demo/SnipRunApplication.java
@@ -0,0 +1,13 @@
+package SnipRun.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnipRunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnipRunApplication.class, args);
+	}
+
+}
